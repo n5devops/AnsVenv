51d98a903e8493bf448844e9b1777d287ef90813
n5devops <117832034+n5devops@users.noreply.github.com>
2022-12-02 15:57:55 +0530

    Update envsetp.sh

--- envsetp.sh
+++ envsetp.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-
 sudo yum install gcc openssl-devel bzip2-devel libffi-devel -y
-sudo wget https://www.python.org/ftp/python/3.9.6/Python-3.9.6.tgz 
-sudo tar xzf Python-3.9.6.tgz 
-cd Python-3.9.6
-sudo ./configure --enable-optimizations 
-sudo make altinstall 
-echo "ENTER DIRECTORY" 
+sudo wget https://www.python.org/ftp/python/3.9.6/Python-3.9.6.tgz
+sudo tar xzf Python-3.9.6.tgz
+cd /home/ec2-user/Python-3.9.6
+sudo ./configure --enable-optimizations
+sudo make altinstall
+cd /home/ec2-user
+echo "ENTER DIRECTORY"
 pwd
 sudo rm -f Python-3.9.6.tgz
-echo "CHECK PYTHON VERSION" 
+echo "CHECK PYTHON VERSION"
 sudo python3.9 --version
-echo "CREATE VENV" 
-python3.9 -m venv env 
+echo "CREATE VENV"
+python3.9 -m venv /home/ec2-user/env
 source env/bin/activate
 pip3 install gunicorn
 pip3 install flask
