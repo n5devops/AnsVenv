d9250720c982ac6a4fd1bc023caad1f92200a103
n5devops <117832034+n5devops@users.noreply.github.com>
2022-12-02 15:16:31 +0530

    Create envsetp.sh

---
+++ envsetp.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+sudo yum install gcc openssl-devel bzip2-devel libffi-devel -y
+sudo wget https://www.python.org/ftp/python/3.9.6/Python-3.9.6.tgz 
+sudo tar xzf Python-3.9.6.tgz 
+cd Python-3.9.6
+sudo ./configure --enable-optimizations 
+sudo make altinstall 
+echo "ENTER DIRECTORY" 
+mkdir pycode
+cd /home/ec2-user/pycode
+pwd
+sudo rm -f Python-3.9.6.tgz
+echo "CHECK PYTHON VERSION" 
+sudo python3.9 --version
+echo "CREATE VENV" 
+python3.9 -m venv env 
+source env/bin/activate
+pip3 install gunicorn
+pip3 install flask
+sudo systemctl start gunicorn
+sudo systemctl enable gunicorn
